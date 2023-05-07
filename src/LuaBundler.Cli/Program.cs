7a57876fe83c85d7e1be7d295da681792d45ab9b
Spiegel <seoksuho@gmail.com>
2023-05-07 13:39:06 +0900

    Added Colurful Console

--- src/LuaBundler.Cli/Program.cs
+++ src/LuaBundler.Cli/Program.cs
@@ -1,4 +1,6 @@
-﻿using LuaBundler.Core;
+﻿using System.Drawing;
+using LuaBundler.Core;
+using Console = Colorful.Console;
 
 if (args.Length < 2)
     return;
@@ -9,11 +11,12 @@ var distPath = args[1];
 var bundler = new LuaFileBundler();
 bundler.RegistryFile += (obj, luaFile) =>
 {
-    Console.WriteLine($"Registry success {luaFile.Name}");
+    
+    Console.WriteLine($"Registry success {luaFile.Name}", Color.GreenYellow);
 };
 bundler.CompleteGenerateFiles += () =>
 {
-    Console.WriteLine("Bundled lua files Complete");
+    Console.WriteLine("Bundled lua files Complete", Color.GreenYellow);
 };
 
 bundler.ToFile(sourcePath, distPath);
